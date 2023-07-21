5a70ffb68c2e77590676a1a9fecec18c57c6d64c
girardiantonio <girardiantonio@gmail.com>
2023-07-21 16:58:26 +0200

    Fix

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ class Telefono {
             console.log('Credito insufficiente per effettuare la chiamata');
         }
     }
+    info(min, id) {
+        let today = new Date();
+        let now = today.toLocaleString();
+        const newCall = {
+            id: id,
+            durata: min,
+            dataOra: now,
+        };
+        this.registroChiamate.push(newCall);
+    }
+    printInfo() {
+        this.registroChiamate.forEach((chiamata) => {
+            console.log("ID:", chiamata.id);
+            console.log("Durata:", chiamata.durata);
+            console.log("Data e Ora:", chiamata.dataOra);
+            console.log("----------------------");
+        });
+    }
     azzeraChiamate() {
         this.numeroChiamate = 0;
     }
@@ -40,12 +58,14 @@ function printData(utente) {
 let utente1 = new Telefono(10, 0); // (credito di partenza, numero di chiamate)
 console.log('---Utente 1---');
 utente1.chiamata(2); // effettuo una chiamata virtuale da 2 minuti
-utente1.chiamata(20); // effettuo una chiamata virtuale da 2 minuti
-utente1.chiamata(20); // effettuo una chiamata virtuale da 2 minuti
+utente1.chiamata(20); // effettuo una chiamata virtuale da 20 minuti
+utente1.chiamata(20); // effettuo una chiamata virtuale da 20 minuti
+utente1.info(2, `5687568265826`);
+utente1.printInfo();
 printData(utente1);
 let utente2 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
 console.log('---Utente 2---');
-utente2.ricarica(10); // effettua una ricarica di 20
+utente2.ricarica(10); // effettua una ricarica di 10
 utente2.chiamata(2); // effettuo una chiamata virtuale da 2 minuti
 utente2.chiamata(20); // effettuo una chiamata virtuale da 20 minuti
 utente2.chiamata(10); // effettuo una chiamata virtuale da 10 minuti
@@ -54,9 +74,9 @@ printData(utente2);
 let utente3 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
 console.log('---Utente 3---');
 utente3.chiamata(2); // effettuo una chiamata virtuale da 2 minuti
-utente3.ricarica(5); // effettua una ricarica di 20
+utente3.ricarica(5); // effettua una ricarica di 5
 utente3.chiamata(20); // effettuo una chiamata virtuale da 20 minuti
-utente3.ricarica(5); // effettua una ricarica di 20
-utente3.chiamata(30); // effettuo una chiamata virtuale da 10 minuti
+utente3.ricarica(5); // effettua una ricarica di 5
+utente3.chiamata(30); // effettuo una chiamata virtuale da 30 minuti
 utente3.chiamata(10); // effettuo una chiamata virtuale da 10 minuti
 printData(utente3);
--- index.ts
+++ index.ts
@@ -2,17 +2,14 @@ interface smartphone {
     credito: number;
     numeroChiamate: number;
     costoMinuto: number
-    registroChiamate: Array<{id:number, durata:number, data:string, ora:string}>;
+    registroChiamate: Array<{id:string, durata:number, dataOra:string}>;
 
     ricarica(euro:number):void
     numero404():string
     getNumeroChiamate():number
     chiamata(min:number):void
     azzeraChiamate():void
-
-
 }
-
 
 class Telefono implements smartphone {
     public credito: number;
@@ -25,7 +22,7 @@ class Telefono implements smartphone {
         this.costoMinuto = 0.20
     }
 
-    public registroChiamate: { id: number; durata: number; data: string; ora: string; }[] = [] 
+    public registroChiamate: { id: string; durata: number; dataOra: string; }[] = [] 
 
     public ricarica(euro: number): void {
         this.credito += euro
@@ -52,10 +49,36 @@ class Telefono implements smartphone {
         }
     }
 
+    public info(min:number,  id: string):void {
+        let today = new Date();
+        let now = today.toLocaleString();
+
+
+        const newCall = {
+            id: id,
+            durata: min, 
+            dataOra:now, 
+          };
+
+
+        this.registroChiamate.push(newCall)
+
+    }
+
+    public printInfo(): void {
+        this.registroChiamate.forEach((chiamata) => {
+          console.log("ID:", chiamata.id);
+          console.log("Durata:", chiamata.durata);
+          console.log("Data e Ora:", chiamata.dataOra);
+          console.log("----------------------");
+        });
+      }
+
+
+
     public azzeraChiamate(): void {
         this.numeroChiamate = 0;
     }
-
 }
 
 function printData(utente: Telefono): void {
@@ -64,33 +87,35 @@ function printData(utente: Telefono): void {
     utente.azzeraChiamate()
     console.log(`Chiamate azzerate, n. chiamate: ${utente.numeroChiamate}`);
     console.log(`Registro chiamate: ${utente.registroChiamate}`);
-  }
+}
   
-  let utente1 = new Telefono(10, 0); // (credito di partenza, numero di chiamate)
-  console.log('---Utente 1---');
-  utente1.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
-  utente1.chiamata(20) // effettuo una chiamata virtuale da 2 minuti
-  utente1.chiamata(20) // effettuo una chiamata virtuale da 2 minuti
-  printData(utente1);
+let utente1 = new Telefono(10, 0); // (credito di partenza, numero di chiamate)
+console.log('---Utente 1---');
+utente1.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
+utente1.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
+utente1.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
+utente1.info(2, `5687568265826`)
+utente1.printInfo()
+printData(utente1);
 
-  let utente2 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
-  console.log('---Utente 2---');
-  utente2.ricarica(10) // effettua una ricarica di 20
-  utente2.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
-  utente2.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
-  utente2.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
-  utente2.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
-  printData(utente2);
+let utente2 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
+console.log('---Utente 2---');
+utente2.ricarica(10) // effettua una ricarica di 10
+utente2.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
+utente2.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
+utente2.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
+utente2.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
+printData(utente2);
 
-  let utente3 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
-  console.log('---Utente 3---');
-  utente3.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
-  utente3.ricarica(5) // effettua una ricarica di 20
-  utente3.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
-  utente3.ricarica(5) // effettua una ricarica di 20
-  utente3.chiamata(30) // effettuo una chiamata virtuale da 10 minuti
-  utente3.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
-  printData(utente3);
+let utente3 = new Telefono(5, 0); // (credito di partenza, numero di chiamate)
+console.log('---Utente 3---');
+utente3.chiamata(2) // effettuo una chiamata virtuale da 2 minuti
+utente3.ricarica(5) // effettua una ricarica di 5
+utente3.chiamata(20) // effettuo una chiamata virtuale da 20 minuti
+utente3.ricarica(5) // effettua una ricarica di 5
+utente3.chiamata(30) // effettuo una chiamata virtuale da 30 minuti
+utente3.chiamata(10) // effettuo una chiamata virtuale da 10 minuti
+printData(utente3);
   
 
 
